c541b3bfad35b55b4111075732e3a87369d4f08f
Pao <ma.paola.tapia@gmail.com>
2023-08-23 22:13:17 -0300

    correccion register

--- src/main/java/com/example/hbsisters/controllers/ClientController.java
+++ src/main/java/com/example/hbsisters/controllers/ClientController.java
@@ -8,10 +8,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.stream.Collectors;
@@ -51,7 +48,7 @@ public class ClientController {
                 .findByEmail(authentication.getName()));
     }
 
-
+    @RequestMapping(path = "/clients", method = RequestMethod.POST)
     public ResponseEntity<Object> register(
 
             @RequestParam String firstName, @RequestParam String lastName,
